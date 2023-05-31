0defc9eefbcd76b3cef8ef8c39e23eface9ae9d7
brandoly-jack <junitrapa@gmail.com>
2023-05-30 20:17:54 -0500

    home creado y estilado para movile

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import {HashRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <HashRouter>
+        <Routes>
+          <Route
+          path='/'
+          element={<Home/>}
+          />
+
+        </Routes>
+      </HashRouter>
+    </>
+  )
+}
+
+export default App
